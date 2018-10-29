0367f1596cbcaa4daa08e5bbb49140d16ffc8930
Leonardo Pereira <leonardopereira519@gmail.com>
2018-10-28 22:41:22 -0300

    initial commit

---
+++ Assets/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+    public Sprite artwork;
+    public new string name;
+    public string description;
+
+    public int health;
+    public int manaCost;
+    public int attack;
+}
---
+++ Assets/CardDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+
+    public Card card;
+
+    public Image artworkImage;
+
+    public Text nameText;
+    public Text descriptionText;
+
+    public Text healthText;
+    public Text manaText;
+    public Text attackText;
+
+    void Start()
+    {
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+
+        artworkImage.sprite = card.artwork;
+
+        manaText.text = card.manaCost.ToString();
+        attackText.text = card.attack.ToString();
+        healthText.text = card.health.ToString();
+    }
+}
